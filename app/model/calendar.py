23e881034e8ecd0ac2999eb3a2702e7f892b286b
CRISTOPHER <geronimosanchezromero@gmail.com>
2024-09-12 10:41:19 -0500

    Class Reminder

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -7,7 +7,16 @@ from app.services.util import generate_unique_id, date_lower_than_today_error, e
 
 
 # TODO: Implement Reminder class here
+@dataclass
+class Reminder:
+    EMAIL: ClassVar[str] = "email"
+    SYSTEM: ClassVar[str] = "system"
 
+    date_time: datetime
+    type: str = EMAIL
+
+    def __str__(self) -> str:
+        return f"Reminder on {self.date_time} of type {self.type}"
 
 # TODO: Implement Event class here
 
